952731154a1f5ea8a9d807921e97be653f225ae0
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 13:40:54 +0100

    "Pawns can move up to 2 spaces."

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -11,20 +11,37 @@ export default class Pawn extends Piece {
     public getAvailableMoves(board: Board) {
         const moves :Array<Square> = [];
         const currentPosition :Square = board.findPiece(this);
+
         if (!board.isValidSquare(currentPosition)) {
             return moves;
         }
+
         if (this.player == Player.WHITE) {
             const forwardMove :Square = new Square(currentPosition.row + 1, currentPosition.col);
             if (board.isValidSquare(forwardMove) && (board.getPiece(forwardMove) == undefined)) {
                 moves.push(forwardMove);
-                return moves;
+
+                if (currentPosition.row == 1) {
+                    const forwardMove2 :Square = new Square(currentPosition.row + 2, currentPosition.col);
+                    if (board.isValidSquare(forwardMove2) && (board.getPiece(forwardMove2) == undefined)) {
+                        moves.push(forwardMove2);
+                    }
+                }
             }
+            return moves;
         }
+
         if (this.player == Player.BLACK) {
             const forwardMove :Square = new Square(currentPosition.row - 1, currentPosition.col);
             if (board.isValidSquare(forwardMove) && (board.getPiece(forwardMove) == undefined)) {
                 moves.push(forwardMove);
+
+                if (currentPosition.row == 6) {
+                    const forwardMove2 :Square = new Square(currentPosition.row - 2, currentPosition.col);
+                    if (board.isValidSquare(forwardMove2) && (board.getPiece(forwardMove2) == undefined)) {
+                        moves.push(forwardMove2);
+                    }
+                }
             }
         }
         return moves;
