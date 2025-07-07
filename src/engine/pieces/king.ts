8caae4b11115e3d2a9ae509a764b62a97582e939
Mihnea Fodor <Mihnea.Fodor@softwire.com>
2025-07-07 11:40:20 +0100

    "Added King movement"

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,31 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const moves :Array<Square> = [];
+        const currentPosition :Square = board.findPiece(this);
+
+        if (!board.isValidSquare(currentPosition)) {
+            return moves;
+        }
+
+        const directions = [
+            [1, 0],    // up
+            [-1, 0],   // down
+            [0, 1],    // right
+            [0, -1],   // left
+            [1, 1],    // up-right
+            [-1, -1],  // down-left
+            [-1, 1],   // down-right
+            [1, -1],   // up-left
+        ];
+
+        for (const [rowMove, colMove] of directions) {
+            const newMove :Square = new Square(currentPosition.row + rowMove,
+                currentPosition.col + colMove);
+            if (board.isValidSquare(newMove) && (board.getPiece(newMove) == undefined)) {
+                moves.push(newMove);
+            }
+        }
+        return moves;
     }
 }
